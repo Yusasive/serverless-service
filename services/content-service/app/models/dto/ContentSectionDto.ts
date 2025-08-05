9ec24bc8358e29b94ce02aefe768532f93818686
Yusuf Abdulahi <yusufabdullahi2910@gmail.com>
2025-08-05 06:27:37 +0100

    services/content-service/

---
+++ services/content-service/app/models/dto/ContentSectionDto.ts
@@ -0,0 +1,60 @@
+import {
+  IsString,
+  IsOptional,
+  IsBoolean,
+  IsNumber,
+  IsObject,
+} from "class-validator";
+
+export class CreateContentSectionDto {
+  @IsString()
+  section_key!: string;
+
+  @IsString()
+  title!: string;
+
+  @IsString()
+  content!: string;
+
+  @IsOptional()
+  @IsString()
+  image_url?: string;
+
+  @IsOptional()
+  @IsObject()
+  metadata?: any;
+
+  @IsOptional()
+  @IsBoolean()
+  is_active?: boolean;
+
+  @IsOptional()
+  @IsNumber()
+  display_order?: number;
+}
+
+export class UpdateContentSectionDto {
+  @IsOptional()
+  @IsString()
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  content?: string;
+
+  @IsOptional()
+  @IsString()
+  image_url?: string;
+
+  @IsOptional()
+  @IsObject()
+  metadata?: any;
+
+  @IsOptional()
+  @IsBoolean()
+  is_active?: boolean;
+
+  @IsOptional()
+  @IsNumber()
+  display_order?: number;
+}
